a92fa5678bc3465060d1813f79ac3c9d4c99aa1a
Rafael Rodriguez <rafael.nc@me.com>
2024-05-20 11:14:38 -0400

    Initial Commit

---
+++ TablesTest/TablesTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TablesTestApp.swift
+//  TablesTest
+//
+//  Created by Rafael Rodriguez on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct TablesTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
